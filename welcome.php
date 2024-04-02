97e49125e3d148965cd73bb862bb138007badefa
Caroline <wanguic49@gmail.com>
2024-04-02 16:11:05 +0300

    reading & writing changes to files

--- welcome.php
+++ welcome.php
@@ -3,9 +3,16 @@
 <body>
 
 <?php
-$myfile = fopen("dictionary.txt", "r+") or die("Unable to open file!");
-echo fread($myfile,filesize("dictionary.txt"));
-fclose($myfile);
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+    $txt = "John Doe\n";
+    fwrite($myfile, $txt);
+    $txt = "Jane Doe\n";
+    fwrite($myfile, $txt);
+    $myfile = fopen("newfile.txt", "r") or die("Unable to open file!");
+    echo fread($myfile,filesize("newfile.txt"));
+    fclose($myfile);
 ?>
+
+
 </body>
 </html>
