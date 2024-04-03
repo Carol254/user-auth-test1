9d341dad2e501970e21d0b2576dbbe8c26f883dc
Caroline <wanguic49@gmail.com>
2024-04-03 16:48:22 +0300

    few add-ons on classes

--- demo-classes.php
+++ demo-classes.php
@@ -1,27 +1,28 @@
 <?php
-    class Fruit{
-        //properties
-        public $name;
-        public $color;
+class Fruit {
+  // Properties
+  public $name;
+  public $color;
 
-        //Methods
-        function set_name($name){
-            $this->name = $name;
-        }
+  // Methods
+  function set_name($name) {
+    $this->name = $name;
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function set_color($color) {
+    $this->color = $color;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
 
-        function get_name(){
-            return $this->name;
-        }
-    }
-
-        $apple = new Fruit();
-        $banana = new Fruit();
-
-        $apple->set_name('Apple');
-        $banana->set_name('Banana');
-
-        echo $apple->get_name();
-        echo "<br>";
-        echo $banana->get_name();
-    
+$apple = new Fruit();
+$apple->set_name('Apple');
+$apple->set_color('Red');
+echo "Name: " . $apple->get_name();
+echo "<br>";
+echo "Color: " . $apple->get_color();
 ?>
