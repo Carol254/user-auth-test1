62b57b24a9fc4ec0d0e8d22d9c2f99b0f8aa9915
Caroline <wanguic49@gmail.com>
2024-04-08 11:40:17 +0300

    sorting db items

--- demo-db-connection.php
+++ demo-db-connection.php
@@ -1,49 +1,26 @@
 <?php
-    $servername = 'localhost';
-    $username = '';
-    $password = '';
-    $dbname = 'myDB';
+  $servername = "localhost";
+  $username = "root";
+  $password = "carolkui";
+  $dbname = "myDB";
 
-    //create connection
-    $conn = new mysqli($servername,$username,$password,$dbname);
-    //check connection
-    if($conn->connect_error){
-        die("Connection failed: " .$conn->connect_error);
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
     }
-
-    echo "Connected successfully";
-
-    //create a database
-
-    // $sql = "CREATE DATABASE myDB";
-    // If($conn->query($sql)=== TRUE){
-    //     echo "Database create successfully";
-    // }else{
-    //     echo "Error creating database: " .$conn->error;
-    // }
-
-    //creating a table
-
-    // $sql = "CREATE TABLE myGuests(
-    //     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    //     firstname VARCHAR(30) NOT NULL,
-    //     lastname VARCHAR(30) NOT NULL,
-    //     email VARCHAR(50),
-    //     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    // )";
-
-    //inserting data into a table
-    $sql = "INSERT INTO myGuests (firstname, lastname, email)
-    VALUES('John', 'Doe', 'john@example.com')";
-
-
-
-    If($conn->query($sql) === TRUE){
-        $last_id = $conn->insert_id;
-        echo "New record created successfully.Last inserted ID is:" .$last_id;
-    }else{
-        echo "Error: ".$sql ."<br>".$conn->error;
-    }
-
-    $conn->close();
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 ?>
